bb2d012abc626c77a0b24e2280034e5c0a8301c7
RikoBgrff <riko.bagirli777@gmail.com>
2021-06-10 14:10:48 +0400

    Add project files.

---
+++ MessengerMediatorPattern/Commands/LeftMessengerCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MessengerMediatorPattern.Entities;
+
+namespace MessengerMediatorPattern.Commands
+{
+    public class LeftMessengerCommand : BaseCommand
+    {
+        private Server _server;
+        private Client _client; 
+        public LeftMessengerCommand(Server server, Client client)
+        {
+            _server = server;
+            _client = client;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter is Window window)
+                window.Close();
+
+
+            _server.Notify(_client, new MessengerEventArgs()
+            {
+                MessengerEventType = MessengerEventType.SomeoneLeft,
+            });
+        }
+    }
+}
